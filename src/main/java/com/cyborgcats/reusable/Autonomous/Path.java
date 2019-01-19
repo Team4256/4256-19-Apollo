e075b78e868e46cd0e6564df4ee14a2bc4b06f4d
CyborgCats4256 <xxx@yyy.zzz>
2019-01-19 09:49:44 -0600

    decided to debug old code in smaller batches

--- src/main/java/com/cyborgcats/reusable/Autonomous/Path.java
+++
@@ -1,8 +0,0 @@
-package com.cyborgcats.reusable.Autonomous;
-
-public interface Path {
-	public boolean increment(final double amount);
-	public double getX();
-	public double getY();
-	public double getIndependentVariable();
-}
