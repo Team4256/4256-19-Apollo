2da9d796cdebb3af2221c7ecb4f688b91cf03166
Vernon Adamson <vernon.adamson@adamson.us>
2019-01-26 10:58:58 -0600

    Adding unit testing

---
+++ src/test/java/org/usfirst/frc/team4256/robot/RobotTest.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team4256.robot;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class RobotTest {
+
+
+    @Test
+    public void robotInitTest() {
+
+assertTrue(true);
+
+    }
+
+}
