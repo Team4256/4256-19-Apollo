e57f1004f7f77ab04f6b8c7accc0488a9bea5543
WCAFRC4256B <cyborgcats4256@wcastl.org>
2019-01-29 20:43:25 -0600

    Updated Parameters

--- src/main/java/org/usfirst/frc/team4256/robot/Parameters.java
+++ src/main/java/org/usfirst/frc/team4256/robot/Parameters.java
@@ -4,15 +4,15 @@ public final class Parameters {
     
     public static final byte GYRO_UPDATE_HZ = 50;
 
-    public static final int ROTATOR_A_ID = 1;
-    public static final int ROTATOR_B_ID = 2;
-    public static final int ROTATOR_C_ID = 3;
-    public static final int ROTATOR_D_ID = 4;
+    public static final int ROTATOR_A_ID = 11;
+    public static final int ROTATOR_B_ID = 12;
+    public static final int ROTATOR_C_ID = 13;
+    public static final int ROTATOR_D_ID = 14;
 
-    public static final int TRACTION_A_ID = 5;
-    public static final int TRACTION_B_ID = 6;
-    public static final int TRACTION_C_ID = 7;
-    public static final int TRACTION_D_ID = 8;
+    public static final int TRACTION_A_ID = 21;
+    public static final int TRACTION_B_ID = 22;
+    public static final int TRACTION_C_ID = 23;
+    public static final int TRACTION_D_ID = 24;
 
     
     
