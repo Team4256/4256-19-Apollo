cff181273c0c821b3dce507eddce9f6670bfb153
CyborgCats4256 <xxx@yyy.zzz>
2019-01-29 18:33:24 -0600

    Added initial swerve testing

--- src/main/java/org/usfirst/frc/team4256/robot/Parameters.java
+++ src/main/java/org/usfirst/frc/team4256/robot/Parameters.java
@@ -3,5 +3,17 @@ package org.usfirst.frc.team4256.robot;
 public final class Parameters {
     
     public static final byte GYRO_UPDATE_HZ = 50;
+
+    public static final int ROTATOR_A_ID = 1;
+    public static final int ROTATOR_B_ID = 2;
+    public static final int ROTATOR_C_ID = 3;
+    public static final int ROTATOR_D_ID = 4;
+
+    public static final int TRACTION_A_ID = 5;
+    public static final int TRACTION_B_ID = 6;
+    public static final int TRACTION_C_ID = 7;
+    public static final int TRACTION_D_ID = 8;
+
+    
     
 }
--- src/main/java/org/usfirst/frc/team4256/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4256/robot/Robot.java
@@ -8,11 +8,18 @@
 package org.usfirst.frc.team4256.robot;
 
 import com.cyborgcats.reusable.Gyro;
+import org.usfirst.frc.team4256.robot.SwerveModule;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 
 
 public class Robot extends TimedRobot {
+
+  private static final SwerveModule moduleA = new SwerveModule(Parameters.ROTATOR_A_ID, false, Parameters.TRACTION_A_ID, false, 0);
+  private static final SwerveModule moduleB = new SwerveModule(Parameters.ROTATOR_B_ID, false, Parameters.TRACTION_B_ID, false, 0);
+  private static final SwerveModule moduleC = new SwerveModule(Parameters.ROTATOR_C_ID, false, Parameters.TRACTION_C_ID, false, 0);
+  private static final SwerveModule moduleD = new SwerveModule(Parameters.ROTATOR_D_ID, false, Parameters.TRACTION_D_ID, false, 0);
+  private static final D_Swerve swerve = new D_Swerve(moduleA, moduleB, moduleC, moduleD);
 
   private static final Gyro gyro = new Gyro(Parameters.GYRO_UPDATE_HZ);//TODO add parameters as a byte
   public static double gyroHeading = 0.0;
@@ -24,6 +31,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     gyro.reset();
+    swerve.init();
   }
 
 
@@ -70,5 +78,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testPeriodic() {
+    moduleA.swivelTo(0);
+    moduleB.swivelTo(0);
+    moduleC.swivelTo(0);
+    moduleD.swivelTo(0);
   }
 }
