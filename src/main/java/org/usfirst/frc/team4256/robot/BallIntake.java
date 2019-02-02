ecdb3f38c535097f639d0095632f42b04d02012b
WCAFRC4256B <cyborgcats4256@wcastl.org>
2019-02-02 14:10:12 -0600

    added BallIntake class

--- src/main/java/org/usfirst/frc/team4256/robot/BallIntake.java
+++ src/main/java/org/usfirst/frc/team4256/robot/BallIntake.java
@@ -1,5 +1,28 @@
 package org.usfirst.frc.team4256.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.cyborgcats.reusable.phoenix.Victor;
+
 public final class BallIntake {
 
+    private boolean hasBall;
+
+    private final Victor ballMotor;
+
+    public BallIntake(int deviceID) {
+        ballMotor = new Victor(deviceID, ControlMode.PercentOutput);
+        hasBall = false;
+    }
+
+    public void slurp() {
+        ballMotor.quickSet(.5);
+    }
+
+    public void spit() {
+        ballMotor.quickSet(-.5);
+    }
+
+    public void stop() {
+        ballMotor.quickSet(0);
+    }
 }
