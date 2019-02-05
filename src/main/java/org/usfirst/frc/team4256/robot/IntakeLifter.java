844a1c99d377135d2231ff529533fdd3c4f3a449
WCAFRC4256B <cyborgcats4256@wcastl.org>
2019-02-05 17:44:49 -0600

    switched talons to victors in intakelifter class

--- src/main/java/org/usfirst/frc/team4256/robot/IntakeLifter.java
+++ src/main/java/org/usfirst/frc/team4256/robot/IntakeLifter.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team4256.robot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.cyborgcats.reusable.phoenix.Encoder;
 import com.cyborgcats.reusable.phoenix.Talon;
+import com.cyborgcats.reusable.phoenix.Victor;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 
@@ -12,41 +13,41 @@ public final class IntakeLifter {
         DOWN, CARGO_SHIP, ROCKET, UP 
     }
 
-
-    private static final double maxPercent = 1.0;
     private static final double gearRatio = 84/18;
 
     //Instance
     private final Talon master;//TODO rename
-    private final Talon followerOne;//TODO rename
-    private final Talon followerTwo;//TODO rename
-    private final Talon followerThree;//TODO rename
-    
-    private final DigitalInput limitSwitchUp;//TODO rename
-    private final DigitalInput limitSwitchDown;//TODO rename
+    private final Victor followerOne;//TODO rename
+    private final Victor followerTwo;//TODO rename
+    private final Victor followerThree;//TODO rename
 
-    private final int masterID;
+    private final boolean followerOneFlipped;
+    private final boolean followerTwoFlipped;
+    private final boolean followerThreeFlipped;
     
     private IntakeLifterPosition currentIntakeLifterPosition; 
 
-    public IntakeLifter(int masterID, int followerOneID, int followerTwoID, int followerThreeID, int limitSwitchUpID, int limitSwitchDownID, boolean masterFlippedSensor) {
+    public IntakeLifter(int masterID, int followerOneID, int followerTwoID, int followerThreeID, boolean masterFlippedSensor, boolean followerOneFlipped, boolean followerTwoFlipped, boolean followerThreeFlipped) {
         master = new Talon(masterID, gearRatio, ControlMode.Position, Encoder.CTRE_MAG_ABSOLUTE, masterFlippedSensor);
-        followerOne = new Talon(followerOneID, ControlMode.Follower);
-        followerTwo = new Talon(followerTwoID, ControlMode.Follower);
-        followerThree = new Talon(followerThreeID, ControlMode.Follower);
-        limitSwitchUp = new DigitalInput(limitSwitchUpID);
-        limitSwitchDown= new DigitalInput(limitSwitchDownID);
+        followerOne = new Victor(followerOneID, ControlMode.Follower);
+        followerTwo = new Victor(followerTwoID, ControlMode.Follower);
+        followerThree = new Victor(followerThreeID, ControlMode.Follower);
 
-        this.masterID = masterID;
+        this.followerOneFlipped = followerOneFlipped;
+        this.followerTwoFlipped = followerTwoFlipped;
+        this.followerThreeFlipped = followerThreeFlipped;
 
         currentIntakeLifterPosition = IntakeLifterPosition.UP;
     }
 
     public void init() {
         master.init();
-        followerOne.init(masterID, maxPercent);
-        followerTwo.init(masterID, maxPercent);
-        followerThree.init(masterID, maxPercent);
+        followerOne.init(master);
+        followerTwo.init(master);
+        followerThree.init(master);
+        followerOne.setInverted(followerOneFlipped);
+        followerTwo.setInverted(followerTwoFlipped);
+        followerThree.setInverted(followerThreeFlipped);
     }
     
 }
