2295b7ca4b06c504ea5615baa4055ca9097bf9d7
frc <frc@westminster-stl.org>
2019-02-11 18:17:29 -0600

    added Encoder check

--- src/main/java/org/usfirst/frc/team4256/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4256/robot/Robot.java
@@ -125,6 +125,7 @@ public class Robot extends TimedRobot {
     //INTAKE LIFTER
     if (!(intakeLifter.isClimbMode())) 
     {//NOT CLIMB MODE
+      intakeLifter.checkForEncoderSpike();
       if (gunner.getRawButtonPressed(Xbox.BUTTON_A)) 
       {//DOWN
         intakeLifter.increment(5.0);
