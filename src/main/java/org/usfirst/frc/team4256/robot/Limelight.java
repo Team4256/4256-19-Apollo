521e22a2c5960b93c25ad6d580f0b3f26e34ffb4
WCAFRC4256B <cyborgcats4256@wcastl.org>
2019-03-15 22:07:22 -0500

    Added even more javadoc

--- src/main/java/org/usfirst/frc/team4256/robot/Limelight.java
+++ src/main/java/org/usfirst/frc/team4256/robot/Limelight.java
@@ -32,6 +32,9 @@ public class Limelight {
 
     }
 
+    /**
+     * A periodically run function that uses vison to compute direction, speed, and spin for swerve in order to score autonomously.
+     */
     public void updateVisionTracking() {
 
         double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0.0);
@@ -53,48 +56,89 @@ public class Limelight {
         commandedSpin = 0.0;
     }
 
+    /**
+     * Changes the LED mode.
+     * @param ledMode the desired ledMode [0, 3].
+     */
     private void changeLEDMode(int ledMode) {
         ledMode = (ledMode >= 0 && ledMode <= 3) ? (ledMode) : (0);
         NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(ledMode);
     }
 
+    /**
+     * Forces the LED off.
+     */
     public void turnLEDOff() {
         changeLEDMode(LED_MODE.OFF.getValue());
     }
 
+    /**
+     * Forces the LED on.
+     */
     public void turnLEDOn() {
         changeLEDMode(LED_MODE.ON.getValue());
     }
 
+    /**
+     * Forces the LED to blink.
+     */
     public void makeLEDBlink() {
         changeLEDMode(LED_MODE.BLINK.getValue());
     }
 
+    /**
+     * Sets the LED to their default value.
+     */
     public void makeLEDDefault() {
         changeLEDMode(LED_MODE.DEFAULT.getValue());
     }
 
+    /**
+     * Changes the pipeline of the vision.
+     * @param pipeline desired pipeline [0, 9].
+     */
     public void changePipeline(int pipeline) {
         pipeline = (pipeline >= 0 && pipeline <= 9) ? (pipeline) : (0);
         NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(pipeline);
     }
 
+    /**
+     * @return
+     * <p><code>True</code> if a valid target is found</p>
+     * <p>and</p>
+     * <p><code>False</code> if a valid target is not found</p>
+     */
     public boolean hasValidTarget() {
         return hasValidTarget;
     }
 
+    /**
+     * @return
+     * <p><code>True</code> if we are aligned with the target</p>
+     * <p>and</p>
+     * <p><code>False</code> if we are not aligned with the target</p>
+     */
     public boolean isAlignedWithTarget() {
         return isAlignedWithTarget;
     }
 
+    /**
+     * @return the <code>commandedDirection</code> for swerve computed in {@link #updateVisionTracking()}.
+     */
     public double getCommandedDirection() {
         return commandedDirection;
     }
 
+    /**
+     * @return the <code>commandedSpeed</code> for swerve computed in {@link #updateVisionTracking()}.
+     */
     public double getCommandedSpeed() {
         return commandedSpeed;
     }
 
+    /**
+     * @return the <code>commandedSpin</code> for swerve computed in {@link #updateVisionTracking()}.
+     */
     public double getCommandedSpin() {
         return commandedSpin;
     }
